e2890b1ca14c5918651e8d8608364083f6ca98b3
jkwon-dev <kwonejk@gmail.com>
2023-12-13 17:02:07 +0900

    Initial Commit

---
+++ AudioPlayerDemo2/AudioPlayerDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerDemo2App.swift
+//  AudioPlayerDemo2
+//
+//  Created by kwon eunji on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
